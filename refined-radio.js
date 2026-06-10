3de328fda94e64e1c395d125bf8d8c4897cf139d
ChuckB4 <113301181+ChuckB4@users.noreply.github.com>
2026-06-09 22:30:44 -0500

    Add files via upload

---
+++ refined-radio.js
@@ -0,0 +1,39 @@
+window.REFINED_RADIO = {
+  "music": {
+    "Acoustic": [
+      {
+        "title": "Beauty Holiness Spanish Guitar",
+        "src": "music/Acoustic/Beauty-Holiness-Spanish-Guitar.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Nylon String Hallelujah",
+        "src": "music/Acoustic/Nylon-String-Hallelujah.mp3",
+        "bpm": 0
+      }
+    ],
+    "Blues": [
+      {
+        "title": "He Lifts the Lowly Male Blues Pop",
+        "src": "music/Blues/He-Lifts-the-Lowly-Male-Blues-Pop.mp3",
+        "bpm": 0
+      }
+    ],
+    "Chill": [
+      {
+        "title": "Fire in My Bones Chill",
+        "src": "music/Chill/Fire-in-My-Bones-Chill.mp3",
+        "bpm": 0
+      }
+    ]
+  },
+  "ids": [
+    "ids/Refined-Recovery-Network.mp3",
+    "ids/Refined-Recovery.mp3",
+    "ids/ttsmaker-file-2026-2-21-3-49-5.mp3"
+  ],
+  "ads": [
+    "ads/Beta-Tester-Invitation.mp3",
+    "ads/Chariot-With-Purpose.mp3"
+  ]
+};
