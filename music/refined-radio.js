884251571545c9a3ab9d990c9db5ba6a502d1e88
ChuckB4 <113301181+ChuckB4@users.noreply.github.com>
2026-06-09 23:49:54 -0500

    Add files via upload

---
+++ music/refined-radio.js
@@ -0,0 +1,201 @@
+window.REFINED_RADIO = {
+  "music": {
+    "Acoustic": [
+      {
+        "title": "Beauty Holiness Spanish Guitar",
+        "src": "music/Acoustic/Beauty-Holiness-Spanish-Guitar.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Nylon String Hallelujah",
+        "src": "music/Acoustic/Nylon-String-Hallelujah.mp3",
+        "bpm": 0
+      }
+    ],
+    "Blues": [
+      {
+        "title": "He Lifts the Lowly Male Blues Pop",
+        "src": "music/Blues/He-Lifts-the-Lowly-Male-Blues-Pop.mp3",
+        "bpm": 0
+      }
+    ],
+    "Chill": [
+      {
+        "title": "Fire in My Bones Chill",
+        "src": "music/Chill/Fire-in-My-Bones-Chill.mp3",
+        "bpm": 0
+      }
+    ],
+    "Country": [
+      {
+        "title": "Granpa was a Preacher Man",
+        "src": "music/Country/Granpa-was-a-Preacher-Man.mp3",
+        "bpm": 0
+      }
+    ],
+    "EDM-Club": [
+      {
+        "title": "Bounce in His grace l",
+        "src": "music/EDM-Club/Bounce-in-His-grace-l.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Fever",
+        "src": "music/EDM-Club/Fever.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Grace Fever",
+        "src": "music/EDM-Club/Grace-Fever.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "I Will Walk On Water Remix",
+        "src": "music/EDM-Club/I-Will-Walk-On-Water-Remix.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "KINGDOM DRILL NO EXCUSES",
+        "src": "music/EDM-Club/KINGDOM-DRILL-NO-EXCUSES.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Kingdom beat",
+        "src": "music/EDM-Club/Kingdom-beat.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Proven Armor Radio edm",
+        "src": "music/EDM-Club/Proven-Armor-Radio-edm.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "River of Grace 808",
+        "src": "music/EDM-Club/River-of-Grace-808.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Spoken For euro rave bounce",
+        "src": "music/EDM-Club/Spoken-For-euro-rave-bounce.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Step Strong euro hop",
+        "src": "music/EDM-Club/Step-Strong-euro-hop.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Straped x Fire in My Bones Mashup 0 89x",
+        "src": "music/EDM-Club/Straped-x-Fire-in-My-Bones-Mashup-0-89x.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Stronger Than the Pain ClubMix",
+        "src": "music/EDM-Club/Stronger-Than-the-Pain-ClubMix.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Stronger Than the Pain Rap version",
+        "src": "music/EDM-Club/Stronger-Than-the-Pain-Rap-version.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Stronger than The Pain Tear The Roof Club Mix",
+        "src": "music/EDM-Club/Stronger-than-The-Pain-Tear-The-Roof-Club-Mix.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Tear the Roof x Stronger Than the Pain Mashup",
+        "src": "music/EDM-Club/Tear-the-Roof-x-Stronger-Than-the-Pain-Mashup.mp3",
+        "bpm": 0
+      }
+    ],
+    "R&B": [
+      {
+        "title": "Hidden Glory R B",
+        "src": "music/R&B/Hidden-Glory-R-B.mp3",
+        "bpm": 0
+      }
+    ],
+    "Radio": [
+      {
+        "title": "Beauty Holiness Radio",
+        "src": "music/Radio/Beauty-Holiness-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Come Alive Chains Fall Down Radio",
+        "src": "music/Radio/Come-Alive-Chains-Fall-Down-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Double Portion Radio",
+        "src": "music/Radio/Double-Portion-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Fall on Us Radio",
+        "src": "music/Radio/Fall-on-Us-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Fire in My Bones Radio",
+        "src": "music/Radio/Fire-in-My-Bones-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Overcome in Rest Radio",
+        "src": "music/Radio/Overcome-in-Rest-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Pain and Rpride Radio",
+        "src": "music/Radio/Pain-and-Rpride-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Proven Armor Radio",
+        "src": "music/Radio/Proven-Armor-Radio.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "You Hold Mde Up Radio",
+        "src": "music/Radio/You-Hold-Mde-Up-Radio.mp3",
+        "bpm": 0
+      }
+    ],
+    "Trance": [
+      {
+        "title": "Hallelujah Lamb Trance",
+        "src": "music/Trance/Hallelujah-Lamb-Trance.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "Standing in the fire Trance",
+        "src": "music/Trance/Standing-in-the-fire-Trance.mp3",
+        "bpm": 0
+      }
+    ],
+    "Worship": [
+      {
+        "title": "A Vapor Breath",
+        "src": "music/Worship/A-Vapor-Breath.mp3",
+        "bpm": 0
+      },
+      {
+        "title": "AFTER THE STORM",
+        "src": "music/Worship/AFTER-THE-STORM.mp3",
+        "bpm": 0
+      }
+    ]
+  },
+  "ids": [
+    "ids/Refined-Recovery-Network.mp3",
+    "ids/Refined-Recovery.mp3",
+    "ids/ttsmaker-file-2026-2-21-3-49-5.mp3"
+  ],
+  "ads": [
+    "ads/Beta-Tester-Invitation.mp3",
+    "ads/Chariot-With-Purpose.mp3"
+  ]
+};
